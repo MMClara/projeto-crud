33e610d8750c00918a2cee8f5bdd022f5e412863
MMClara <maria.costa1397@aluno.ce.gov.br>
2025-03-31 17:27:59 -0300

    Rota para remover criada

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
@@ -38,5 +38,11 @@ public class ProdutoServico {
     }
   }
 
-  
+  public ResponseEntity<RespostaModelo> remover(long codigo) {
+    pr.deleteById(codigo);
+    rm.setResposta("O produto foi removido com sucesso!");
+    return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+
+  }
+
 }
