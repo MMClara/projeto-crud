b1613da1590dbcc2b6469e91abad9230652c6f75
MMClara <maria.costa1397@aluno.ce.gov.br>
2025-03-31 17:20:53 -0300

    criação do metodo listar de serviço

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
@@ -1,5 +1,16 @@
 package br.com.projetocrud.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.repositorio.ProdutoRepositorio;
+
 public class ProdutoServico {
-  
+  @Autowired
+  private ProdutoRepositorio pr;
+
+  public Iterable<ProdutoModelo> listar() {
+    return pr.findAll();
+
+  }
 }
