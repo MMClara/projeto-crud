22ab2fab75af6eaf31fc6d01a50e5442a96d9013
MMClara <maria.costa1397@aluno.ce.gov.br>
2025-04-12 16:39:34 -0300

    Projeto front criado

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.projetocrud.produtos.modelo.ProdutoModelo;
@@ -31,6 +32,11 @@ public class ProdutoControle {
     return ps.cadastrarAlterar(pm, "cadastrar");
 
   }
+
+  @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
 
   @GetMapping("/listar")
   public Iterable<ProdutoModelo> listar() {
